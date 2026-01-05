aff68850fe4ade9cf0b89e5d650ad4287207eda2
jadedbay <bayne.jaded@gmail.com>
2026-01-06 00:12:31 +1100

    remove load util method

--- Utils/Util.cs
+++ Utils/Util.cs
@@ -1,28 +1,6 @@
-using System.IO;
-using System.Reflection;
-using UnityEngine;
-
 namespace NeedleArts.Utils;
 
 public static class Util {
-    public static Texture2D LoadTextureFromAssembly(string resourceName) {
-        var asm = Assembly.GetExecutingAssembly();
-
-        using Stream? stream = asm.GetManifestResourceStream(resourceName);
-        if (stream == null) {
-            NeedleArtsPlugin.Log.LogError($"Resource not found: {resourceName}");
-            return null;
-        }
-
-        using MemoryStream ms = new();
-        stream.CopyTo(ms);
-        byte[] data = ms.ToArray();
-
-        Texture2D tex = new Texture2D(2, 2);
-        tex.LoadImage(data);
-        return tex;
-    }
-
     public static tk2dSpriteAnimationClip CopyClip(tk2dSpriteAnimationClip clip, string newName) {
         return new tk2dSpriteAnimationClip {
             name = newName,
