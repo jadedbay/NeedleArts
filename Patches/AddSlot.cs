0ad1ec6359d4ee4231972d99fc57bb2d91b0af1d
jadedbay <bayne.jaded@gmail.com>
2026-01-16 05:15:05 +1100

    needleartslot class

--- Patches/AddSlot.cs
+++ Patches/AddSlot.cs
@@ -11,57 +11,17 @@ public class AddSlot {
     private const float SlotPosY = -3.85f;
     private const float SlotXGap = 1.0f;
 
-    private static GameObject upSlot;
-    private static GameObject neutralSlot;
-    private static GameObject downSlot;
+    private static List<NeedleArtSlot> slots;
     
     [HarmonyPatch(typeof(InventoryFloatingToolSlots), nameof(InventoryFloatingToolSlots.Awake))]
     [HarmonyPostfix]
     private static void SpawnSlot(InventoryFloatingToolSlots __instance) {
-        upSlot = Object.Instantiate(
-            __instance.transform.Find("Defend Slot").gameObject,
-            __instance.transform
-        );
-        upSlot.name = "NeedleArt Slot";
-        upSlot.SetActive(false);
-        upSlot.transform.localScale = new Vector3(0.6f, 0.6f);
-
-        neutralSlot = Object.Instantiate(upSlot, __instance.transform);
-        neutralSlot.name = "NeedleArt Slot";
-        downSlot = Object.Instantiate(upSlot, __instance.transform);
-        downSlot.name = "NeedleArt Slot";
+        slots = [
+            new NeedleArtSlot(__instance, AttackToolBinding.Up),
+            new NeedleArtSlot(__instance, AttackToolBinding.Neutral),
+            new NeedleArtSlot(__instance, AttackToolBinding.Down)
+        ];
         
-        var upSlotObject = upSlot.GetComponent<InventoryToolCrestSlot>();
-        var neutralSlotObject = neutralSlot.GetComponent<InventoryToolCrestSlot>();
-        var downSlotObject = downSlot.GetComponent<InventoryToolCrestSlot>();
-        
-        var cursedArtSlot = Object.Instantiate(
-            __instance.transform.Find("Cursed Socket Top").gameObject,
-            __instance.transform
-        );
-        cursedArtSlot.transform.SetLocalPosition2D(SlotPosX, SlotPosY);
-        
-        var upConfigSlot = new InventoryFloatingToolSlots.Slot {
-            SlotObject = upSlotObject,
-            Type = NeedleArtsPlugin.ToolType(),
-            Id = "NeedleArtsSlot_Up",
-            CursedSlot = cursedArtSlot,
-        };
-        
-        var neutralConfigSlot = new InventoryFloatingToolSlots.Slot {
-            SlotObject = neutralSlotObject,
-            Type = NeedleArtsPlugin.ToolType(),
-            Id = "NeedleArtsSlot_Neutral",
-            CursedSlot = cursedArtSlot,
-        };
-        
-        var downConfigSlot = new InventoryFloatingToolSlots.Slot {
-            SlotObject = downSlotObject,
-            Type = NeedleArtsPlugin.ToolType(),
-            Id = "NeedleArtsSlot_Down",
-            CursedSlot = cursedArtSlot,
-        };
-
         var test = new PlayerDataTest.Test {
             FieldName = "hasChargeSlash",
             Type = PlayerDataTest.TestType.Bool,
@@ -79,7 +39,7 @@ public class AddSlot {
              };
             
             needleArtConfigs.Add(new InventoryFloatingToolSlots.Config {
-                Slots = [..config.Slots, upConfigSlot, neutralConfigSlot, downConfigSlot],
+                Slots = [..config.Slots, ..slots.Select(s => s.slot)],
                 Brackets = [..config.Brackets],
                 PositionOffset = config.PositionOffset,
                 Condition = condition,
@@ -87,7 +47,7 @@ public class AddSlot {
         }
 
         var baseConfig = new InventoryFloatingToolSlots.Config {
-            Slots = [upConfigSlot, neutralConfigSlot, downConfigSlot],
+            Slots = [..slots.Select(s => s.slot)],
             Brackets = [],
             PositionOffset = new Vector2(0.0f, 0.0f),
             Condition = new PlayerDataTest {
@@ -100,39 +60,21 @@ public class AddSlot {
         };
 
         __instance.configs = [baseConfig, ..__instance.configs, ..needleArtConfigs];
-        
-        var data = PlayerData.instance.ExtraToolEquips.GetData("NeedleArtsSlot");
-        if (string.IsNullOrEmpty(data.EquippedTool)) {
-            PlayerData.instance.ExtraToolEquips.SetData("NeedleArtsSlot", new ToolCrestsData.SlotData());
-        }
     }
     
     [HarmonyPatch(typeof(InventoryFloatingToolSlots), nameof(InventoryFloatingToolSlots.Evaluate))]
     [HarmonyPostfix]
     private static void SetSlotValues(InventoryFloatingToolSlots __instance) {
-        if (neutralSlot == null) return;
+        if (slots.IsNullOrEmpty()) return;
 
         var attackSlots = Resources.FindObjectsOfTypeAll<GameObject>()
             .Where(go => go.name == "Attack Slot(Clone)" && go.transform.parent.name == "Hunter")
             .ToList();
-        
-        SetAnimator(attackSlots, upSlot, AttackToolBinding.Up);
-        upSlot.GetComponent<InventoryToolCrestSlot>().slotInfo.AttackBinding = AttackToolBinding.Up;
-        SetAnimator(attackSlots, downSlot, AttackToolBinding.Down);
-        upSlot.GetComponent<InventoryToolCrestSlot>().slotInfo.AttackBinding = AttackToolBinding.Down;
-        
-        neutralSlot.transform.Find("Background Group/Background").GetComponent<Animator>().runtimeAnimatorController =
-            __instance.transform.Find("Defend Slot/Background Group/Background").GetComponent<Animator>()
-                .runtimeAnimatorController;
-    }
+        attackSlots.Add(__instance.transform.Find("Defend Slot").gameObject);
 
-    private static void SetAnimator(List<GameObject> attackSlots, GameObject artSlot, AttackToolBinding attackBinding) {
-        var attackSlot = attackSlots.First(s =>
-            s.GetComponent<InventoryToolCrestSlot>().slotInfo.AttackBinding == attackBinding);
-        
-        artSlot.transform.Find("Background Group/Background").GetComponent<Animator>().runtimeAnimatorController =
-            attackSlot.transform.Find("Background Group/Background").GetComponent<Animator>()
-                .runtimeAnimatorController;
+        foreach (var slot in slots) {
+            slot.Evaluate(attackSlots);
+        }
     }
 
     [HarmonyPatch(typeof(InventoryItemGrid), nameof(InventoryItemGrid.PositionGridItems))]
@@ -186,3 +128,39 @@ public class AddSlot {
     }
 }
 
+internal class NeedleArtSlot {
+    public GameObject gameObject;
+    public InventoryFloatingToolSlots.Slot slot;
+    public AttackToolBinding binding;
+    
+    public NeedleArtSlot(InventoryFloatingToolSlots inventoryFloatingToolSlots, AttackToolBinding binding) {
+        this.binding = binding;
+        
+        gameObject = Object.Instantiate(
+            inventoryFloatingToolSlots.transform.Find("Defend Slot").gameObject,
+            inventoryFloatingToolSlots.transform
+        );
+        gameObject.name = "NeedleArt Slot";
+        gameObject.SetActive(false);
+        gameObject.transform.localScale = new Vector3(0.6f, 0.6f);
+        
+        slot = new InventoryFloatingToolSlots.Slot {
+            SlotObject = gameObject.GetComponent<InventoryToolCrestSlot>(),
+            Type = NeedleArtsPlugin.ToolType(),
+            Id = $"NeedleArtsSlot_{binding.ToString()}",
+            CursedSlot = Object.Instantiate(
+                inventoryFloatingToolSlots.transform.Find("Cursed Socket Top").gameObject,
+                inventoryFloatingToolSlots.transform
+            )
+        };
+    }
+
+    public void Evaluate(List<GameObject> attackSlots) {
+        gameObject.GetComponent<InventoryToolCrestSlot>().slotInfo.AttackBinding = binding;
+        var attackSlot = attackSlots.First(s => s.GetComponent<InventoryToolCrestSlot>().slotInfo.AttackBinding == binding);
+        
+        gameObject.transform.Find("Background Group/Background").GetComponent<Animator>().runtimeAnimatorController =
+            attackSlot.transform.Find("Background Group/Background").GetComponent<Animator>()
+                .runtimeAnimatorController;
+    }
+}
